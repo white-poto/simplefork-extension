1218b8b719e2dc551cb49163e3ef0a41eabef207
huyanping <hypxm@qq.com>
2015-09-16 14:24:30 +0800

    debug

--- tests/ProcessTest.php
+++ tests/ProcessTest.php
@@ -13,7 +13,7 @@ class ProcessTest extends TestSuite
     }
 
     public function testProperties(){
-        $process = new SimpleFork\Process();
+        $process = new Process();
         $reflect = new ReflectionObject($process);
         $this->assertTrue($reflect->hasConstant("BEFORE_START"));
         $this->assertTrue($reflect->hasConstant("BEFORE_EXIT"));
