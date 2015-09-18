d4cf336dcbb2d966449af8aa434746e1fe2b98b4
huyanping <hypxm@qq.com>
2015-09-18 10:40:59 +0800

    test

--- simplefork.c
+++ simplefork.c
@@ -143,9 +143,12 @@ PHP_METHOD(Process, __construct)
 {
 	zval *execution = NULL;
 	char *execution_name = NULL;
-	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS(), "z", &execution)){
+	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS(), "|z", &execution)){
 		RETURN_FALSE;
 	}
+	if (Z_TYPE_P(execution) == IS_NULL) {
+        RETURN_NULL();
+    }
 
 	if(!zend_is_callable(execution, 0, &execution_name)){
 		zend_throw_exception(simplefork_exception_entry, "execution param must be callable",0 TSRMLS_CC);
