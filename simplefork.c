7ed801550060e0b3591ea04abd5dc8fa58d68fe0
huyanping <hypxm@qq.com>
2016-07-25 11:02:11 +0800

    debug

--- simplefork.c
+++ simplefork.c
@@ -165,7 +165,7 @@ PHP_METHOD(Process, __construct)
 {
 	zval *runnable = NULL;
 	zval *process_name = NULL;
-	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS(), "|z!z!", &runnable, &process_name)){
+	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS(), "|zz", &runnable, &process_name)){
 		RETURN_FALSE;
 	}
 	if (runnable == NULL) {
