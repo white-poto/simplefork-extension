6b3dad805b14688b1e0cfdbef08fb13e714c34b4
huyanping <hypxm@qq.com>
2016-07-25 12:21:18 +0800

    debug

--- simplefork.c
+++ simplefork.c
@@ -204,7 +204,7 @@ PHP_METHOD(Process, name)
         RETURN_FALSE;
     }
 
-    print name;
+    printf(name);
     if(!name)
     {
         zval *process_name = zend_read_property(process_class_entry, getThis(), "name", sizeof("name")-1, 0 TSRMLS_DC);
