8c1c2c4c24b3af678625219852b1ea58ada569c4
huyanping <hypxm@qq.com>
2016-07-25 11:23:11 +0800

    debug

--- simplefork.c
+++ simplefork.c
@@ -173,6 +173,7 @@ PHP_METHOD(Process, __construct)
     }
 
 	zend_update_property(process_class_entry, getThis(), "runnable", sizeof("runnable")-1, runnable TSRMLS_CC);
+	zend_update_property(process_class_entry, getThis(), "name", sizeof("name")-1, process_name TSRMLS_CC);
 }
 /* }}} */
 
