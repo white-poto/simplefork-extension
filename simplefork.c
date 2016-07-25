170327e24da36cde6bfe16c9578dbd74439c7670
huyanping <hypxm@qq.com>
2016-07-25 11:22:03 +0800

    debug

--- simplefork.c
+++ simplefork.c
@@ -172,8 +172,6 @@ PHP_METHOD(Process, __construct)
         zend_throw_exception(simplefork_exception_entry, "execution param must be callable", 0 TSRMLS_CC);
     }
 
-	zend_update_property(process_class_entry, getThis(), "runnable", sizeof("runnable")-1, runnable TSRMLS_CC);
-	zend_update_property(process_class_entry, getThis(), "name", sizeof("name")-1, process_name TSRMLS_CC);
 }
 /* }}} */
 
