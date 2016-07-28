c9447216c8e9b027c0fa5105fbb04bd79980ea35
huyanping <hypxm@qq.com>
2016-07-28 18:01:05 +0800

    tests

--- tests/scripts/process.php
+++ tests/scripts/process.php
@@ -9,7 +9,7 @@
 $process = new SimpleFork\Process(function() {
     echo getmypid(), ". sub_process", PHP_EOL;
     //sleep(20);
-    //exit();
+    exit();
 }, "process_name");
 //var_dump($process);
 //var_dump($process->name());
