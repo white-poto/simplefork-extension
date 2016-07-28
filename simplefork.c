12d4da87f79d4a1194fcc5e4604c3ff82e7ecaea
huyanping <hypxm@qq.com>
2016-07-28 15:31:46 +0800

    tests

--- simplefork.c
+++ simplefork.c
@@ -462,7 +462,7 @@ PHP_METHOD(Process, wait)
 	    php_printf("runnnnnnnnnnn\n");
         if (call_user_function_ex(
             CG(function_table), &getThis(), &method_name,
-            &retval_ptr, 0, NULL, 0, NULL TSRMLS_CC
+            retval_ptr, 0, NULL, 0, NULL TSRMLS_CC
         ) == FAILURE
         ) {
             zend_throw_exception(simplefork_exception_entry, "call isRunning failed", 0 TSRMLS_CC);
@@ -472,8 +472,9 @@ PHP_METHOD(Process, wait)
         int is_running = Z_BVAL_P(running);
         php_printf("running:%ld\n", is_running);
         if(is_running == 0){
-            zval_ptr_dtor(&retval_ptr);
-            zval_dtor(&method_name);
+            *return_value=*retval_ptr;
+                    zval_copy_ctor(return_value);
+                    zval_ptr_dtor(&retval_ptr);
             RETURN_TRUE;
         }
 
